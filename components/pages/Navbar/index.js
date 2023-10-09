9935bece0f3566ea2cccd7a1e6eb5af26a002c0c
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-10-09 23:52:43 +1300

    Fix today's router bug.

--- components/pages/Navbar/index.js
+++ components/pages/Navbar/index.js
@@ -7,12 +7,12 @@ function Navbar() {
         <nav className="navbarContainer">
             <div className="logo">
                 {/* add a logo picture */}
-                <img className="logoImage" src="./images/logo.png" alt="logo" />
+                <img className="logoImage" src="/images/logo.png" alt="logo" />
             </div>
             <div className="navLinks">
                 <Link href="/">Home</Link>
                 <Link href="/about">About</Link>
-                <Link href="/today">Todo List</Link>
+                <Link href="/app/today">Todo List</Link>
                 <Link href="/login">Login</Link>
             </div>
         </nav>
@@ -20,4 +20,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
