acf0bbbd1f2d9ff2351a97b9dc73c9dd57c0307a
ryanwooa <119087910+ryanwooa@users.noreply.github.com>
2023-11-13 13:05:09 +1300

    update setting card with more choice

--- components/layout/Settings.js
+++ components/layout/Settings.js
@@ -1,15 +1,29 @@
-import "./NewTaskCard.scss";
+import "./Settings.scss";
 
-export default function SettingsCard({ subMenu, closeHandler  }) {
+export default function SettingsCard({ subMenu, closeHandler }) {
   const containerClickHandler = (event) => {
     event.stopPropagation();
   };
 
   return (
-    <div className="click_close_cover grey_cover " onClick={closeHandler} >
-      <div className="new_task_container" onClick={containerClickHandler}>
-        <div>Settings</div>
-        <div>{subMenu}</div>
+    <div className="click_close_cover grey_cover " onClick={closeHandler}>
+      <div className="setting_container" onClick={containerClickHandler}>
+        <div className="setting-left">
+          <header>Settings</header>
+          <div>Search</div>
+          <div>
+            <ul>
+              <li>Account</li>
+              <li>General</li>
+              <li>Theme</li>
+              <li>Sidebar</li>
+            </ul>
+          </div>
+        </div>
+        <form className="setting-right">
+          <header>{subMenu}</header>
+          <div>content</div>
+        </form>
       </div>
     </div>
   );
