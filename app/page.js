784d026ccfeb4ac4c9adcfd594d7248da8ab010c
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-11-19 20:42:40 +1300

    Refactor SCSS files.

--- app/page.js
+++ app/page.js
@@ -1,19 +1,21 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
-
   return (
     <div>
       <div className="slogan">
         <h1 className="slogan_h1">Organize your work</h1>
         <h1 className="slogan_h1">and life, finally.</h1>
-        <p className="slogan_p">Become focused, organized, and calm with Todoist. The world’s #1</p>
+        <p className="slogan_p">
+          Become focused, organized, and calm with Todoist. The world’s #1
+        </p>
         <p className="slogan_p">task manager and to-do list app.</p>
         <Link href="/login">
-          <button className='get_started'>Get Started</button>
+          <button className="standard-button">Get Started</button>
         </Link>
       </div>
       <div className="background">
+        <img src="/background.png" alt="background" />
       </div>
     </div>
   );
