0ae212f17a28d4747ab1bea8cac2e1aa85067321
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-11-21 12:39:22 +1300

    Change the SCSS file names to follow rules.

--- components/pages/Navbar/index.js
+++ components/pages/Navbar/index.js
@@ -1,20 +1,20 @@
 // components/Navbar/index.js
-import "../../../styles/scss/navbar.scss";
+import styles from "../../../styles/scss/navbar.module.scss";
 import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className="navbarContainer">
-      <div className="logo">
+    <nav className={styles.navbarContainer}>
+      <div className={styles.logo}>
         {/* add a logo picture */}
-        <img className="logoImage" src="./images/logo.png" alt="logo" />
+        <img className={styles.logoImage} src="./images/logo.png" alt="logo" />
       </div>
-      <div className="navLinks">
+      <div className={styles.navLinks}>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/today">Todo List</Link>
         <Link href="/login">
-          <button className="loginButtom">Start for free</button>
+          <button className={styles.loginButtom}>Start for free</button>
         </Link>
       </div>
     </nav>
