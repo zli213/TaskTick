c948c2b803f555ccf0a4954844bfca84303d581f
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-11-21 23:41:01 +1300

    Add top card of the about page.

--- components/pages/About/index.js
+++ components/pages/About/index.js
@@ -1,11 +1,22 @@
 // About.js
+import styles from "../../../styles/scss/about.module.scss";
 function About() {
-    return (
-        <div>
-            <h1>About Us</h1>
-            <p>This is the about page where you can learn more about our company and mission.</p>
+  return (
+    <div className={styles.container}>
+      <div className={styles.upCard}>
+        <div className={styles.teamImage}>
+          <h1>About Us</h1>
+          <img
+            clasName={styles.teamPhoto}
+            src="/images/teamImage.png"
+            alt="teamImage"
+          />
         </div>
-    );
+        <div className="mapCard"></div>
+      </div>
+      <div className="middleCard"></div>
+    </div>
+  );
 }
 
 export default About;
