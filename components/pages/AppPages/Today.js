d7d8105125c90eff747b58bdd021313bd29e4562
Kelvin Jin <jlmjkfd@hotmail.com>
2023-12-06 22:31:39 +1300

    put add-task component to Today page

--- components/pages/AppPages/Today.js
+++ components/pages/AppPages/Today.js
@@ -4,19 +4,20 @@ import TodoList from "../../application/widgets/TodoList";
 import { DUMMY_TASKS } from "../../../public/dummy-data";
 // import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import AddTask from "../../application/widgets/AddTask";
 
 function Today(props) {
   //fetch today's job
   const router = useRouter();
 
-  if ("settingMenu" in props ) {
+  if ("settingMenu" in props) {
     router.push(`/application/setting/${props.settingMenu}`);
   }
 
   return (
     <>
       <h1>Today</h1>
-
+      <AddTask />
       <TodoList datas={DUMMY_TASKS} />
     </>
   );
