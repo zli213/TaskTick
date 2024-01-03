1cf5fc47f5cd9c90081f9b4f7aa6e2f9765aed8e
kesesek <zmkkma@163.com>
2024-01-03 15:13:31 +0800

    add icons & edit a button

--- components/application/widgets/SettingNotice.js
+++ components/application/widgets/SettingNotice.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../../../styles/scss/components/application/widgets/settingNotice.module.scss";
+import Close from "../../../public/icon/close.svg";
 
 function Notice ({ isOpen, onClose, onConfirm, promptText}) {
     return (
@@ -9,12 +10,9 @@ function Notice ({ isOpen, onClose, onConfirm, promptText}) {
             <div className={styles.noticeContainer}>
                 <header className={styles.noticeHeader}>
                     <label>Discard Changes?</label>
-                    <a onClick={onClose}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="gray" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round">
-                            <line x1="18" y1="6" x2="6" y2="18" />
-                            <line x1="6" y1="6" x2="18" y2="18" />
-                        </svg>
-                    </a>
+                    <button>
+                        <Close onClick={onClose}/>
+                    </button>
                 </header>
                 <p>{promptText}</p>
                 <div className={styles.buttonGroup}>
