8b00f04247b4e64c4f4d37078ac210a39e8bbc38
kesesek <zmkkma@163.com>
2024-01-10 14:44:58 +0800

    edit themes

--- components/pages/Settings/Theme.js
+++ components/pages/Settings/Theme.js
@@ -1,5 +1,44 @@
+import React, {useState,useEffect, useCallback}from "react";
+import { useRouter } from "next/navigation";
+import getTheme from "../../../src/utils/data/getTheme";
+import CloseButton from "../../../public/icon/close.svg";
+import styles from "../../../styles/scss/theme.module.scss";
+import ThemeToggle from "../../application/widgets/ThemeToggle";
+
 const SettingTheme = () => {
-  return <div>This is Theme Setting page</div>;
+  const router = useRouter();
+  const [theme, storedTheme] = useState("");
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userTheme = await getTheme("johndoe123");//need to be modified
+      storedTheme(userTheme);
+    };
+    fetchData();
+  }, []);
+
+  const onDismiss = useCallback(() => {
+    const currentPage = localStorage.getItem("lastPage");
+    router.push(`/application/${currentPage}`, { scroll: false });
+    router.refresh();
+  }, [router]);
+
+  return (
+    <>
+    <div className={styles.container}>
+    <header>
+        <h4>Account Information</h4>
+        <button onClick={onDismiss}>
+          <CloseButton/>
+        </button>
+      </header>
+      <ThemeToggle
+        selectedTheme={theme}
+      />
+    </div>
+    </>
+  );
 };
 
 export default SettingTheme;
