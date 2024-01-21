de585f5d3fd4adedc1ad5f0a2ba17483cb940c30
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2024-01-21 13:44:38 +1300

    Change styles in thirdPartySignInButtons.js.

--- components/pages/Signin/ThirdPartySignInButtons.js
+++ components/pages/Signin/ThirdPartySignInButtons.js
@@ -5,7 +5,6 @@
  */
 import React from "react";
 import Icon from "../../application/widgets/Icon";
-// import styles from "../../../styles/scss/signin.module.scss";
 
 const ThirdPartySignInButtons = ({ handleSignIn, styles }) => {
   return (
@@ -13,28 +12,15 @@ const ThirdPartySignInButtons = ({ handleSignIn, styles }) => {
       <div className={styles.dividerWithText}>
         <span>OR</span>
       </div>
-      <div className={styles.google}>
-        <a
-          className={styles.google}
-          style={{ backgroundColor: "#55acee" }}
-          onClick={() => handleSignIn("google")}
-          role="button"
-        >
-          <Icon type="google" className={styles.icon} />
-          Continue with Google
-        </a>
-      </div>
-      <div className={styles.github}>
-        <a
-          className={styles.github}
-          style={{ backgroundColor: "#55acee" }}
-          onClick={() => handleSignIn("github")}
-          role="button"
-        >
-          <Icon type="github" className={styles.icon} />
-          Continue with GitHub
-        </a>
-      </div>
+      <button className={styles.google} onClick={() => handleSignIn("google")}>
+        <Icon type="google" className={styles.icon} />
+        Continue with Google
+      </button>
+
+      <button className={styles.github} onClick={() => handleSignIn("github")}>
+        <Icon type="github" className={styles.icon} />
+        Continue with GitHub
+      </button>
     </div>
   );
 };
