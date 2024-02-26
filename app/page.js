01c86c39a9815c757abbba5785c7b6725dc09733
ryanwooa <119087910+ryanwooa@users.noreply.github.com>
2024-02-27 11:19:47 +1300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
           <h1 className="slogan_h1">Organize your work</h1>
           <h1 className="slogan_h1">and life, finally.</h1>
           <p className="slogan_p">
-            Become focused, organized, and calm with Todoist. The world’s #1
+            Become focused, organized, and calm with TaskTick. The world’s #1
           </p>
           <p className="slogan_p">task manager and to-do list app.</p>
           <Link href="/auth/register">
