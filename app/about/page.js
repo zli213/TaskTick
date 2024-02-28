25312890331f7b3ebc68c5c29f7603460be29b76
ryanwooa <119087910+ryanwooa@users.noreply.github.com>
2024-02-28 17:25:19 +1300

    #66

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,14 @@
 "use client";
 import AboutPage from "../../components/pages/About/index";
 import Gallery from "../../components/pages/About/GalleryComponent";
+import FooterPage from "../../components/pages/About/widgets/FooterPage";
 
 function About() {
   return (
     <div className="aboutPage">
       <AboutPage />
       <Gallery />
+      <FooterPage />
     </div>
   );
 }
